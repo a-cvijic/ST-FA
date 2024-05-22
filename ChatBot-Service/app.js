06cad430206716994b469f7bc4ae7c398e4d2248
acvijic <aleksa.cvijic@student.um.si>
2024-05-23 00:55:36 +0200

    Port & Naming update

--- ChatBot-Service/app.js
+++ ChatBot-Service/app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 app.use("/chat", chatRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- Recipe-Service/RecipeMain.js
+++ Recipe-Service/RecipeMain.js
@@ -32,6 +32,7 @@ webpush.setVapidDetails(
 // Use routes
 app.use("/", recipeRoutes);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3003;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
