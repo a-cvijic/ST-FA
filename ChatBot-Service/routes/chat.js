9fe9654a949f064a72ad92529252a37cf4126770
acvijic <aleksa.cvijic@student.um.si>
2024-05-23 15:26:28 +0200

    Protect Routes

--- ChatBot-Service/routes/chat.js
+++ ChatBot-Service/routes/chat.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { OpenAI } = require("openai");
 const ChatMessage = require("../models/ChatMessages");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const secretKey = process.env.SECRET_KEY;
 
 const router = express.Router();
 
@@ -10,7 +12,20 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-router.post("/", async (req, res) => {
+// Middleware function to authenticate token
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+// Apply token authentication middleware to the chatbot route
+router.post("/", authenticateToken, async (req, res) => {
   const { message } = req.body;
 
   if (!message) {
