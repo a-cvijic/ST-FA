3fefd074fdd68a920ecb2c922c4dd5f9d0b619c4
PV528 <whyisalltaken218@gmail.com>
2024-05-30 18:34:09 +0200

    pocistil public

--- Front-End/public/scripts/logout.js
+++
@@ -1,8 +0,0 @@
-// Function to handle logout
-const handleLogout = () => {
-    localStorage.clear(); // Clear local storage
-    window.location.href = 'index.html'; // Redirect to the homepage or login page
-};
-
-// Event listener for the logout button
-document.getElementById('logout-button').addEventListener('click', handleLogout);
