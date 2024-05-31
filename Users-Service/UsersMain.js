db84b42c38e45e78799e33ff00cb0fdd5b839e9d
PV528 <whyisalltaken218@gmail.com>
2024-06-01 00:32:34 +0200

    dodal caching

--- Users-Service/UsersMain.js
+++ Users-Service/UsersMain.js
@@ -1,30 +1,22 @@
 const express = require('express');
-const connectDB = require('./config/config');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authRoutes = require('./routes/users');
+require('dotenv').config();
 
 const app = express();
+const PORT = 3010;
 
-<<<<<<< HEAD
-connectDB();
-=======
 mongoose.connect(process.env.MONGO_URI_USERS);
 const db = mongoose.connection;
->>>>>>> origin/main
 
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', () => {
+    console.log('Connected to database');
+});
 app.use(express.json());
-<<<<<<< HEAD
-
-const userRoutes = require('./routes/UsersRoutes');
-app.use('/users', userRoutes);
-
-const authRoutes = require('./middleware/autentification');
-app.use('/auth', authRoutes);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-=======
 app.use(cors());
 app.use('/auth', authRoutes);
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
 });
->>>>>>> origin/main
