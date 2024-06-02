d9812ea0ba036fae84f960937ea486d20079cb52
acvijic <aleksa.cvijic@student.um.si>
2024-06-02 21:28:19 +0200

    Home-Page recipe fix.

--- Front-End/src/views/Home/home.js
+++ Front-End/src/views/Home/home.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import './home.css';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./home.css";
 
-const authURL = 'http://localhost:3010/auth';
-const trainingsURL = 'http://localhost:3004/trainings';
-const exercisesURL = 'http://localhost:3000/exercises';
-const recipesURL = 'http://localhost:3003/recipes';
+const authURL = "http://localhost:3010/auth";
+const trainingsURL = "http://localhost:3004/trainings";
+const exercisesURL = "http://localhost:3000/exercises";
+const recipesURL = "http://localhost:3003/recipes";
 
 const Home = () => {
-  const [userName, setUserName] = useState('');
+  const [userName, setUserName] = useState("");
   const [featuredTrainings, setFeaturedTrainings] = useState([]);
   const [featuredExercises, setFeaturedExercises] = useState([]);
   const [featuredRecipes, setFeaturedRecipes] = useState([]);
@@ -17,7 +17,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem("token");
       const userName = await getUserNameFromToken(token);
       setUserName(userName);
 
@@ -43,8 +43,8 @@ const Home = () => {
       });
       return response.data.name;
     } catch (error) {
-      console.error('Napaka pri pridobivanju imena iz žetona:', error);
-      return '';
+      console.error("Napaka pri pridobivanju imena iz žetona:", error);
+      return "";
     }
   };
 
@@ -66,11 +66,22 @@ const Home = () => {
     <div className="home-container">
       <br></br>
 
-      <h2>Dobrodošli, <a href='#' onClick={() => navigate(`/profile`)}>{userName}</a>!</h2>
+      <h2>
+        Dobrodošli,{" "}
+        <a href="#" onClick={() => navigate(`/profile`)}>
+          {userName}
+        </a>
+        !
+      </h2>
 
-      <p>Želite živeti bolj zdravo življenje ampak ne veste kje začeti in potrebujete pomoč? Predlagamo da preizkusite novo orodje:</p>
+      <p>
+        Želite živeti bolj zdravo življenje ampak ne veste kje začeti in
+        potrebujete pomoč? Predlagamo da preizkusite novo orodje:
+      </p>
       <div className="link-button">
-        <Link to="/chat" className="quick-link">AI Klepet</Link>
+        <Link to="/chat" className="quick-link">
+          AI Klepet
+        </Link>
       </div>
 
       <br></br>
@@ -80,14 +91,18 @@ const Home = () => {
         <br></br>
         <div className="featured-items">
           {featuredTrainings.length === 0 ? (
-            <p style={{ color: 'grey' }}>Ni podatkov za prikaz</p>
+            <p style={{ color: "grey" }}>Ni podatkov za prikaz</p>
           ) : (
             featuredTrainings.map((training) => (
               <div key={training._id} className="card-user">
                 <h3>{training.name}</h3>
                 <p>{training.description}</p>
-                <p><strong>Trajanje:</strong> {training.total_duration} minut</p>
-                <p><strong>Kalorije:</strong> {training.total_calories}</p>
+                <p>
+                  <strong>Trajanje:</strong> {training.total_duration} minut
+                </p>
+                <p>
+                  <strong>Kalorije:</strong> {training.total_calories}
+                </p>
               </div>
             ))
           )}
@@ -101,14 +116,18 @@ const Home = () => {
         <br></br>
         <div className="featured-items">
           {featuredExercises.length === 0 ? (
-            <p style={{ color: 'grey' }}>Ni podatkov za prikaz</p>
+            <p style={{ color: "grey" }}>Ni podatkov za prikaz</p>
           ) : (
             featuredExercises.map((exercise) => (
               <div key={exercise._id} className="card-user">
                 <h3>{exercise.name}</h3>
                 <p>{exercise.description}</p>
-                <p><strong>Trajanje:</strong> {exercise.duration} minut</p>
-                <p><strong>Kalorije:</strong> {exercise.calories}</p>
+                <p>
+                  <strong>Trajanje:</strong> {exercise.duration} minut
+                </p>
+                <p>
+                  <strong>Kalorije:</strong> {exercise.calories}
+                </p>
               </div>
             ))
           )}
@@ -122,22 +141,32 @@ const Home = () => {
         <br></br>
         <div className="featured-items">
           {featuredRecipes.length === 0 ? (
-            <p style={{ color: 'grey' }}>Ni podatkov za prikaz</p>
+            <p style={{ color: "grey" }}>Ni podatkov za prikaz</p>
           ) : (
             featuredRecipes.map((recipe) => (
               <div key={recipe._id} className="card-user">
                 <h3>{recipe.name}</h3>
                 <p>{recipe.description}</p>
-                <p><strong>Kalorije:</strong> {recipe.calories}</p>
+                <p>
+                  <strong>Sestavine:</strong> {recipe.ingredients.join(", ")}
+                </p>
+                <p>
+                  <strong>Kalorije:</strong> {recipe.calories}
+                </p>
               </div>
             ))
           )}
         </div>
       </section>
 
-      <section className="motivational-section" style={{ backgroundColor: 'white', border: '3px solid #FED467' }}>
+      <section
+        className="motivational-section"
+        style={{ backgroundColor: "white", border: "3px solid #FED467" }}
+      >
         <h1>Motivacijski kotek</h1>
-        <p><i>"The only bad workout is the one that didn’t happen."</i></p>
+        <p>
+          <i>"The only bad workout is the one that didn’t happen."</i>
+        </p>
         <br></br>
       </section>
     </div>
